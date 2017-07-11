40a9de36d7da1931d0c2aa2b5c6fc239f551d5ab
wangyuebin <wangyb@ecqun.com>
2017-07-11 14:07:50 +0800

    modify bug

--- app/src/main/java/com/cyanbirds/tanlove/fragment/TabPersonalFragment.java
+++ app/src/main/java/com/cyanbirds/tanlove/fragment/TabPersonalFragment.java
@@ -176,6 +176,10 @@ public class TabPersonalFragment extends Fragment implements GeocodeSearch.OnGeo
 	TextView mCity;
 	@BindView(R.id.is_vip)
 	ImageView mIsVip;
+	@BindView(R.id.tv_friend)
+	TextView mTvFriend;
+	@BindView(R.id.card_friend)
+	CardView mCardFriend;
 
 	private AMap aMap;
 	private UiSettings mUiSettings;
@@ -300,6 +304,13 @@ public class TabPersonalFragment extends Fragment implements GeocodeSearch.OnGeo
 				} else {
 					mGiftText.setVisibility(View.GONE);
 					mGiftCard.setVisibility(View.GONE);
+				}
+				if (AppManager.getClientUser().isShowVip) {
+					mCardFriend.setVisibility(View.VISIBLE);
+					mTvFriend.setVisibility(View.VISIBLE);
+				} else {
+					mCardFriend.setVisibility(View.GONE);
+					mTvFriend.setVisibility(View.GONE);
 				}
 			}
 		}
