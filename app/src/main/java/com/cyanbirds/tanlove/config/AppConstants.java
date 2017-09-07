e20a69040a2400893bc97ee9e04a5d6ca1a2534d
wangyuebin <wangyb@ecqun.com>
2017-09-07 15:31:35 +0800

    modify bug

--- app/src/main/java/com/cyanbirds/tanlove/config/AppConstants.java
+++ app/src/main/java/com/cyanbirds/tanlove/config/AppConstants.java
@@ -14,6 +14,7 @@ public class AppConstants {
 	
 	public static final String BASE_URL = "http://120.77.65.198/TanLoveServer/";
 //	public static final String BASE_URL = "http://192.168.1.100/TanLoveServer/";
+//	public static final String BASE_URL = "http://10.0.108.198:8080/TanLoveServer/";
 
 	/**
 	 * 密码加密密匙
