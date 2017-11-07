ffd93aa57c228775b4b3607f1f9d9c6dc75f1620
wangyuebin <wangyb@ecqun.com>
2017-11-07 13:16:44 +0800

    modify bug

--- app/src/main/java/com/cyanbirds/tanlove/config/AppConstants.java
+++ app/src/main/java/com/cyanbirds/tanlove/config/AppConstants.java
@@ -12,7 +12,7 @@ import com.xiaomi.account.openauth.XiaomiOAuthConstants;
  */
 public class AppConstants {
 	
-	public static final String BASE_URL = "http://120.77.65.198/TanLoveServer/";
+	public static final String BASE_URL = "http://119.23.144.11/TanLoveServer/";
 //	public static final String BASE_URL = "http://192.168.1.100/TanLoveServer/";
 //	public static final String BASE_URL = "http://10.0.108.198:8080/TanLoveServer/";
 
