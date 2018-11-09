4399708be8ddacb40297acd460ee4baddd17b26e
wangyb <395044952@qq.com>
2018-11-09 14:42:46 +0800

    modify bug

--- app/src/main/java/com/cyanbirds/tanlove/manager/AppManager.java
+++ app/src/main/java/com/cyanbirds/tanlove/manager/AppManager.java
@@ -27,6 +27,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.LineNumberReader;
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -206,31 +207,31 @@ public class AppManager {
 	 * @return
 	 */
 	public static String getDeviceId() {
-		String deviceId = "";
+		String serial = null;
+		String m_szDevIDShort = "35" +
+				Build.BOARD.length()%10+ Build.BRAND.length()%10 +
+
+				Build.CPU_ABI.length()%10 + Build.DEVICE.length()%10 +
+
+				Build.DISPLAY.length()%10 + Build.HOST.length()%10 +
+
+				Build.ID.length()%10 + Build.MANUFACTURER.length()%10 +
+
+				Build.MODEL.length()%10 + Build.PRODUCT.length()%10 +
+
+				Build.TAGS.length()%10 + Build.TYPE.length()%10 +
+
+				Build.USER.length()%10 ; //13 位
 		try {
-			// 获取ID
-			TelephonyManager tm = (TelephonyManager) mContext
-					.getSystemService(Context.TELEPHONY_SERVICE);
-			String id = tm.getDeviceId();
-			// 获取mac地址
-			String macSerial = null;
-			String str = "";
-			Process pp = Runtime.getRuntime().exec(
-					"cat /sys/class/net/wlan0/address ");
-			InputStreamReader ir = new InputStreamReader(pp.getInputStream());
-			LineNumberReader input = new LineNumberReader(ir);
-			for (; null != str;) {
-				str = input.readLine();
-				if (str != null) {
-					macSerial = str.trim();
-					break;
-				}
-			}
-			deviceId = "Android_" + id + "_" + macSerial;
-		} catch (Exception e) {
-			e.printStackTrace();
+			serial = android.os.Build.class.getField("SERIAL").get(null).toString();
+			//API>=9 使用serial号
+			return new UUID(m_szDevIDShort.hashCode(), serial.hashCode()).toString();
+		} catch (Exception exception) {
+			//serial需要一个初始化
+			serial = "serial"; // 随便一个初始化
 		}
-		return deviceId;
+		//使用硬件信息拼凑出来的15位号码
+		return new UUID(m_szDevIDShort.hashCode(), serial.hashCode()).toString();
 	}
 
 	/**
