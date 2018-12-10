c713a750de398f73469b60753804a84d471daa00
wangyb <395044952@qq.com>
2018-12-10 15:26:01 +0800

    modify bug

--- app/src/main/java/com/cyanbirds/tanlove/config/AppConstants.java
+++ app/src/main/java/com/cyanbirds/tanlove/config/AppConstants.java
@@ -12,7 +12,7 @@ package com.cyanbirds.tanlove.config;
 public class AppConstants {
 	
 	public static final String BASE_URL = "http://119.23.144.11/TanLoveServer/";
-//	public static final String BASE_URL = "http://192.168.1.107:8080/TanLoveServer/";
+//	public static final String BASE_URL = "http://192.168.1.100:8080/TanLoveServer/";
 
 	/**
 	 * 密码加密密匙
